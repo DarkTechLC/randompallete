c9a7495de6f8d13559099f7393ec032e880a1a08
DarkTechLC <cliciosa14@gmail.com>
2020-05-22 11:34:30 -0300

    Add function to block color area

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
 const colorAreas = document.querySelectorAll('.color-area');
+const btnsLock = document.querySelectorAll('.btn-lock');
 const btnRefresh = document.querySelector('#btn-refresh');
 
 const generateRandomHexadecimal = () => {
   const randomNumber = Math.random() * Math.random();
-  const hexadecimal = randomNumber.toString(16).substr(2, 6);
+  const hexadecimal = randomNumber.toString(16).substr(6, 6);
   return `#${hexadecimal}`;
 }
 
-const copyToClipboard = (element, value) => {
-  element.addEventListener('click', () => {
+const lockColorArea = () => {
+  btnsLock.forEach(btnLock => {
+    btnLock.addEventListener('click', () => {
+      let locked = btnLock.parentElement.getAttribute('data-lock-color');
+      let lockIcon;
+
+      [locked, lockIcon] = locked === 'false'
+        ? [true, 'img/lock.svg']
+        : [false, 'img/unlock.svg'];
+
+      btnLock.firstElementChild.setAttribute('src', lockIcon);
+
+      btnLock.parentElement.setAttribute('data-lock-color', locked);
+    });
+  });
+}
+
+const copyToClipboard = (element) => {
+  element.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const value = element.children[1].textContent;
     const fakeInput = document.createElement('input');
     document.body.appendChild(fakeInput);
     fakeInput.value = value;
@@ -18,19 +38,25 @@ const copyToClipboard = (element, value) => {
   });
 }
 
-const changeBackgroundOfEachColorArea = () => {
+const functionsToColorAreas = () => {  
   colorAreas.forEach(colorArea => {
-    const hexadecimal = generateRandomHexadecimal();
-    colorArea.style.background = hexadecimal;
-    colorArea.children[1].textContent = hexadecimal;
-    copyToClipboard(colorArea, hexadecimal);
+    const locked = colorArea.getAttribute('data-lock-color');
+    
+    if (locked === 'false') {
+      const hexadecimal = generateRandomHexadecimal();
+      colorArea.style.background = hexadecimal;
+      colorArea.children[1].textContent = hexadecimal;
+    }
+
+    copyToClipboard(colorArea);
   });
 }
 
-changeBackgroundOfEachColorArea();
+functionsToColorAreas();
+lockColorArea();
 
-btnRefresh.addEventListener('click', changeBackgroundOfEachColorArea);
+btnRefresh.addEventListener('click', functionsToColorAreas);
 
 window.addEventListener('keydown', (event) => {
-  if (event.key === ' ') changeBackgroundOfEachColorArea();
+  if (event.key === ' ') functionsToColorAreas();
 });
